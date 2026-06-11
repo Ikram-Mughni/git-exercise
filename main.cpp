17f92d5912930e12bc2d7e3c3d6f71ed17ee268e
Ikram-Mughni <ikrammughni1011@gmail.com>
2026-06-11 21:09:23 +0700

    mencetak segitiga dengan while loop

--- main.cpp
+++ main.cpp
@@ -5,4 +5,13 @@ using namespace std;
 int main()
 {
     cout << "CETAK SEGITIGA\n";
+    int i = 0;
+    while(i < 5) {
+        int j = 0;
+        while (j < i) {
+            cout << "* ";
+            j++;
+        }
+        cout << endl;
+    }
 }
